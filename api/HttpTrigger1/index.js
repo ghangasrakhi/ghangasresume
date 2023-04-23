96a02e2a945a2536b3866202ed475c30fe7a4777
ghangasrakhi <ghangas.rakhi@gmail.com>
2023-04-23 20:52:55 +1000

    function modified

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -1,13 +1,28 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, req, doc) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // Initialize an empty array to hold the names
+    let names_list = [];
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+    // Loop through each resume and append the name to the array
+    doc.forEach(resume => {
+        names_list.push(resume.name);
+    });
+
+    // Select a random value from the array
+    let random_value = names_list[Math.floor(Math.random() * names_list.length)];
+
+    // Set the headers to allow cross-origin requests
+    let headers = {
+        'Access-Control-Allow-Origin': '*',  // replace * with your domain name
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
     };
-}
+
+    // Return the random value as an HTTP response
+    context.res = {
+        headers: headers,
+        status: 200,
+        body: random_value
+    };
+};
